757001d249cca64aef8b1d80e99f7b53068f5764
Noor B.A <noor.bahr.aluloom@gmail.com>
2020-11-29 22:05:08 -0800

    finalized wave 1

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,6 +11,26 @@ class UsersController < ApplicationController
   def login_form
   end
 
+  def create
+    auth_hash = request.env["omniauth.auth"]
+
+    user = User.find_by(uid: auth_hash[:uid], provider: "github")
+    if user
+      flash[:success] = "Logged in as returning user #{user.username}"
+    else
+      user = User.build_from_github(auth_hash)
+
+      if user.save
+        flash[:success] = "Logged in as new user #{user.username}"
+      else
+        flash[:error] = "Could not create new user account: #{user.errors.messages}"
+        return redirect_to root_path
+      end
+    end
+    session[:user_id] = user.id
+    return redirect_to root_path
+  end
+
   def login
     username = params[:username]
     if username and user = User.find_by(username: username)
@@ -38,6 +58,13 @@ class UsersController < ApplicationController
     session[:user_id] = nil
     flash[:status] = :success
     flash[:result_text] = "Successfully logged out"
+    redirect_to root_path
+  end
+
+  def destroy
+    session[:user_id] = nil
+    flash[:success] = "Successfully logged out!"
+
     redirect_to root_path
   end
 end
